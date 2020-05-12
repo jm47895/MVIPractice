6c08dbd80faaef4ce43191600c0109bf4f705657
jm47895 <jordan.madrigal@gmail.com>
2020-05-12 03:00:49 -0500

    Add MainActivity, MainFrgament, Menu, and BlogPost and User model data classes

---
+++ app/src/main/java/com/jordanmadrigal/mvipractice/ui/main/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.jordanmadrigal.mvipractice.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jordanmadrigal.mvipractice.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        showMainFragment()
+    }
+
+    fun showMainFragment(){
+        supportFragmentManager.beginTransaction()
+            .replace(
+                R.id.fragment_container,
+                MainFragment(), "MainFragment")
+            .commit()
+    }
+}
