2dbcb2274b66ac721a97afd142cad9c9bf486a46
jm47895 <jordan.madrigal@gmail.com>
2020-05-13 03:34:51 -0500

    Add dummy null live data. Add main state event class. Add live data to main state event with switch.

--- app/src/main/java/com/jordanmadrigal/mvipractice/ui/main/MainViewModel.kt
+++ app/src/main/java/com/jordanmadrigal/mvipractice/ui/main/MainViewModel.kt
@@ -2,12 +2,35 @@ package com.jordanmadrigal.mvipractice.ui.main
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.jordanmadrigal.mvipractice.ui.main.state.MainStateEvent
+import com.jordanmadrigal.mvipractice.ui.main.state.MainStateEvent.*
 import com.jordanmadrigal.mvipractice.ui.main.state.MainViewState
+import com.jordanmadrigal.mvipractice.util.AbsentLiveData
 
 class MainViewModel : ViewModel(){
 
+    private val _stateEvent: MutableLiveData<MainStateEvent> = MutableLiveData()
     private val _viewState: MutableLiveData<MainViewState> = MutableLiveData()
 
     val viewState : LiveData<MainViewState> get() = _viewState
+
+    val dataState: LiveData<MainViewState> = Transformations
+        .switchMap(_stateEvent){stateEvent ->
+            when(stateEvent){
+
+                is GetBlogPostsEvent -> {
+                    AbsentLiveData.create<MainViewState>()
+                }
+
+                is GetUserEvent -> {
+                    AbsentLiveData.create<MainViewState>()
+                }
+
+                is None -> {
+                    AbsentLiveData.create<MainViewState>()
+                }
+            }
+        }
 }
